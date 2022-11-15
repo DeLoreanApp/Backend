d945c76403a6c5962917568e2f0ebfea08d61ae3
bobokrut <egor-evlampiev@mail.ru>
2022-11-15 17:23:16 +0100

    Big fix

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import Session
 from bcrypt import hashpw, checkpw, gensalt
 
-from db import Base
+from ..db import Base
 
 class User(Base):
     __tablename__ = "users"
@@ -16,43 +16,33 @@ class User(Base):
 def get_user_by_id(db: Session, user_id: int) -> User | None:
     return db.query(User).filter(User.id == user_id).first()
 
-def get_user_by_email_or_username(db: Session, email: str = None, username: str = None) -> User | None:
+def get_user_by_username(db: Session, username) -> User | None:
 
-    if username and email:
-        raise ValueError("Either email or username can be specified. Not both")
+    return db.query(User).filter(User.username == username).first()
 
-    if username:
-        return db.query(User).filter(User.username == username).first()
 
-    if email:
-        return db.query(User).filter(User.email == email).first()
-
-    raise ValueError("Email or username must be specified")
-
-def create_user(db: Session, user) -> None:
+def create_user(db: Session, user):
 
     password = hashpw(user.password.encode("utf8"), gensalt())
     db_user = User(email=user.email, hashed_password=password, username=user.username)
     db.add(db_user)
     db.commit()
-    # db.refresh(db_user)
-    return
+    db.refresh(db_user)
+    return db_user
 
 def auth(db: Session, user) -> bool:
 
-    if user.email and user.username or not user.email and not user.username:
-        raise ValueError()
+    if "@" in user.email_or_username:
 
-    if user.email:
-        user_db = db.query(User).filter(User.email == user.email).first()
+        user_db = db.query(User).filter(User.email == user.email_or_username).first()
     else:
-        user_db = db.query(User).filter(User.username == user.username).first()
+        user_db = db.query(User).filter(User.username == user.email_or_username).first()
 
     if not user_db:
-        return False
+        return None
 
     if checkpw(user.password.encode("utf8"), user_db.hashed_password):
-        return True
+        return user_db
 
-    return False
+    return None
 
