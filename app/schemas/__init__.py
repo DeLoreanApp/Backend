8bf9c34cffab44f393c16cd5257733721dd2406a
bobokrut <egor-evlampiev@mail.ru>
2022-12-15 04:16:00 +0100

    leaderboard fix

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .general import ResponseSuccess, ResponseError
 from .user import UserFull, UserLogin, UserMinimal, UserRegister, UserResponse
 from .monuments import Monument
+from .user import UserFull, UserLogin, UserMinimal, UserRegister, UserResponse, UserResponseFull, LeaderBoard
